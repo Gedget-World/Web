1a1d18902657e5355ac4fe8fab9d14d5ad21532f
krishnaKantVishWakarma-titan <krishnakantvish.24@gmail.com>
2026-02-21 11:24:03 +0530

    add header label

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -48,6 +48,9 @@ export function SiteHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-white/95 backdrop-blur supports-backdrop-filter:bg-white/60">
+      <div className="text-sm text-center text-white bg-black py-1">
+        Development
+      </div>
       <div className="container flex h-16 items-center justify-between gap-4 px-4 md:px-8">
         <Link href="/" className="flex items-center space-x-2">
           <span className="text-2xl font-bold text-slate-900">StyleHub</span>
